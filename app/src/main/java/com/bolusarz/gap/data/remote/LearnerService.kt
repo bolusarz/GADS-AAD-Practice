5769d7537a2b877442dd3cb854ab987631d93aa5
Bolusarz <bolusarz@gmail.com>
2020-09-06 22:58:49 +0100

    Added lifecycle extensions

---
+++ app/src/main/java/com/bolusarz/gap/data/remote/LearnerService.kt
@@ -0,0 +1,15 @@
+package com.bolusarz.gap.data.remote
+
+import com.bolusarz.gap.data.models.Learner
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface LearnerService {
+
+    @GET("hours")
+    suspend fun getLearningLeaders(): Response<List<Learner>>
+
+    @GET("skilliq")
+    suspend fun getSkillLeaders(): Response<List<Learner>>
+
+}
