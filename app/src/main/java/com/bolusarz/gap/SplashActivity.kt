8a7f6b98b9efd234902787ede7c3bf0431089314
Bolusarz <bolusarz@gmail.com>
2020-09-11 18:22:25 +0100

    Create Splash Screen

--- app/src/main/java/com/bolusarz/gap/SplashActivity.kt
+++ app/src/main/java/com/bolusarz/gap/SplashActivity.kt
@@ -1,11 +1,13 @@
 package com.bolusarz.gap
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
     }
 }
